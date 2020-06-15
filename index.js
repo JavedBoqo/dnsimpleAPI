a2ab7dd3ce59b22f1ca418d6a04d7218358d765e
Javed Iqbal <javedboqo@gmail.com>
2020-06-15 20:44:20 +0500

    Add more objects

--- index.js
+++ index.js
@@ -11,21 +11,68 @@ const Token     = 'DNSIMPLE TOKEN';
 const AccountID = "DNSIMPLE ACCOUNT ID";
 const ZoneName  = 'test.com';  
 const APIUrl    = `https://api.dnsimple.com/v2/${AccountID}/zones/${ZoneName}/records/`;
- 
+const Params    = process.argv.slice(2);
+
+if(Params.length < 4) {
+    console.log(`Parameters missing: {TYPE} {NAME} {IP} {TTL}`);
+    return;
+}
+
+const DNSType = Params[0].toUpperCase();
+const DNSName = Params[1];
+const DNSIP   = Params[2];
+
+//***** Check for Valid TTL
+let TTL         = Params[3];
+let ttlValue    = 0;
+switch((Params[3]).toString().toUpperCase()) {
+    case '1M':
+    case '10M':
+        TTL         = parseInt(TTL.replace("M",""));
+        ttlValue    = TTL*60;
+    break;
+    case '1H':
+    case '2H':
+    case '4H':
+    case '8H':
+    case '12H':
+        TTL         = parseInt(TTL.replace("H",""));
+        ttlValue    = TTL*60*60;
+        break;
+    case '1D':
+    case '3D':
+        TTL         = parseInt(TTL.replace("D",""));
+        ttlValue    = TTL*24*60*60;
+    break;
+}    
+
+const DnsDetail = {
+    record_type :   DNSType,
+    name        :   DNSName,
+    type        :   DNSType,
+    content     :   DNSIP,
+    ttl         :   ttlValue,
+    priority    :   10
+}
+if(ttlValue <= 0) {
+    console.log("Invalid TTL value");
+    return;
+}
+
 const requestHeaders = {
     'Authorization' : 'Bearer ' + Token,
     'Accept'        : 'application/json',
     'Content-Type'  : 'application/json'
 }
 
-const DnsDetail = {
-    record_type :   "A",
-    name        :   "mydns",
-    type        :   "A",
-    content     :   "8.8.8.1",
-    ttl         :   600,
-    priority    :   10
-}
+// const DnsDetail = {
+//     record_type :   "A",
+//     name        :   "mytest",
+//     type        :   "A",
+//     content     :   "8.8.8.1",
+//     ttl         :   600,
+//     priority    :   10
+// }
 
 const Options =    {
     url     :   APIUrl,
@@ -45,3 +92,4 @@ request.post(
      console.log(`Success: ${body}`);
     }
 );
+
